609896a70c2b6925ef15255353c676e5aed3f641
Bangar Mahammed Ghouse <m.ghouse@aletihad.com>
2026-09-24 19:17:42 +0530

    updated encironment in frontend

--- Frontend/SmartInvoicePro.Web/src/environments/environment.ts
+++ Frontend/SmartInvoicePro.Web/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  // apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://smartinvoicepro-api-fkcuesfxdtf9bve7.centralindia-01.azurewebsites.net/api',
 };
